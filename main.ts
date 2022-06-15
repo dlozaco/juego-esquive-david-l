0216c28ef541e9bc27d99e5d1db3ac68319ddac7
dlozaco <106948180+dlozaco@users.noreply.github.com>
2022-06-15 10:14:33 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,18 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     sprite.change(LedSpriteProperty.X, 1)
 })
+function opponent () {
+    malox = randint(0, 4)
+    malo = game.createSprite(malox, 0)
+    basic.pause(1000)
+}
 let malo: game.LedSprite = null
 let malox = 0
 let sprite: game.LedSprite = null
 basic.clearScreen()
 sprite = game.createSprite(2, 4)
 basic.forever(function () {
-    malox = randint(0, 4)
-    malo = game.createSprite(malox, 0)
-    basic.pause(1000)
+    opponent()
     if (malo.isTouching(sprite)) {
         sprite.delete()
         malo.delete()
@@ -23,18 +26,18 @@ basic.forever(function () {
     sprite.set(LedSpriteProperty.X, randint(1, 3))
 })
 basic.forever(function () {
+    if (game.isGameOver()) {
+        sprite.delete()
+        malo.delete()
+    }
+})
+basic.forever(function () {
     if (sprite.get(LedSpriteProperty.X) == 4) {
         sprite.set(LedSpriteProperty.X, 0)
         basic.pause(1000)
     } else if (sprite.get(LedSpriteProperty.X) == 0) {
         sprite.set(LedSpriteProperty.X, 4)
         basic.pause(1000)
-    }
-})
-basic.forever(function () {
-    if (game.isGameOver()) {
-        sprite.delete()
-        malo.delete()
     }
 })
 loops.everyInterval(200, function () {
